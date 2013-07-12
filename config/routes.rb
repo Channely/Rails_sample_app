f28bfb7d1dc6c83f69fc426f44ec8e238a8cb22a
Channely <qijie29896@gmail.com>
2013-07-12 16:15:44 +0800

    chapter6

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
 
 
-  match '/signup',    to: 'users#new'
+  match '/signup',  to: 'users#new'
 
 
   match '/help',    to: 'static_pages#help'
